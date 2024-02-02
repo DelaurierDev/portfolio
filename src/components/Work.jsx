846476ad5bb7a5c96efc988c4485441abf1fa75a
DelaurierDev <delaurier4@gmail.com>
2024-02-01 23:29:57 -0700

    adding a project to the work section

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import WorkImg from '../assets/projects/workImg.jpeg'
 import Power from '../assets/projects/powerthreads.png'
+import Thritter from '../assets/projects/thritter.png'
 const Work = () => {
   return (
     <div name = "work" className='w-full md:h-screen text-gray-300 bg-[#0a192f]'>
@@ -26,16 +27,16 @@ const Work = () => {
                         </div>
                     </div>
                 </div>
-                <div style={{backgroundImage: `url(${WorkImg})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+                <div style={{backgroundImage: `url(${Thritter})`}} className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
                     <div className='opacity-0 duration-300 group-hover:opacity-100'>
                         <span className='text-2xl font-bold text-white tracking-wider'>
-                            React JS Application
+                            Flask Social Media
                         </span>
                         <div className='pt-8 text-center'>
                             <a href="/">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Demo</button>
                             </a>
-                            <a href="/">
+                            <a href="https://github.com/DelaurierDev/thritter_app">
                                 <button className='text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg'>Code</button>
                             </a>
                         </div>
